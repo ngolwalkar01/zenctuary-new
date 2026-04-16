e0291957b17c2c41b8643b92a3bb0780b1198881
Navneet Golwalkar <ngolwalkar@yahoo.com>
2026-04-16 10:28:41 +0530

    Try fixing new carouse block styling to show the expected visibilty of cards.

--- zenctuary-theme-starter/blocks/premium-edge-peek-carousel/index.js
+++ zenctuary-theme-starter/blocks/premium-edge-peek-carousel/index.js
@@ -3,6 +3,7 @@ import { registerBlockType } from '@wordpress/blocks';
 import metadata from './block.json';
 import Edit from './edit';
 import save from './save';
+import 'swiper/css';
 import './style.scss';
 
 registerBlockType( metadata.name, {
--- zenctuary-theme-starter/build/premium-edge-peek-carousel/index.asset.php
+++ zenctuary-theme-starter/build/premium-edge-peek-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0cb20e63e392ca3b3da6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '882500106a08a7a53bdb');
