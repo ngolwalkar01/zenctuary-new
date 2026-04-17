e73c0f7d5485ed52bec7e97aadc80b210faf6b51
Navneet Golwalkar <ngolwalkar@yahoo.com>
2026-04-17 22:09:31 +0530

    Updates for contact map block for UI changes.

--- zenctuary-theme-starter/build/contact-map/index.asset.php
+++ zenctuary-theme-starter/build/contact-map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e0f0627bd8bf242d84e4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e1c986139d1c7c7519ab');
