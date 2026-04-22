7743bf29a5a099ddba316086e2637e2d852073fb
Navneet Golwalkar <ngolwalkar@yahoo.com>
2026-04-22 12:36:47 +0530

    Restored hero block

--- zenctuary-theme-starter/build/zenctuary-hero/index.asset.php
+++ zenctuary-theme-starter/build/zenctuary-hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9f098abdb6a040187aa4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2ebb978bfa0f6a524f3d');
