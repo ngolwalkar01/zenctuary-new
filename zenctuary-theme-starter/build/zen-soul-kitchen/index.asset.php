450b3b99ae64e09a6f645cdf5a666c5ae83e545f
developervinit <vinit91.dev@gmail.com>
2026-04-23 15:07:28 +0530

    fix: zen soul kitchen product fetch

--- zenctuary-theme-starter/build/zen-soul-kitchen/index.asset.php
+++ zenctuary-theme-starter/build/zen-soul-kitchen/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a1f02cb60b60403a274d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fcf3aa07d235e2b200bb');
