1ba71847f06daa8c2aa3e702844f41d74c2f501a
Navneet Golwalkar <ngolwalkar@yahoo.com>
2026-06-26 15:28:06 +0530

    Improve mobile my account navigation flow

--- zenctuary-theme-starter/woocommerce/myaccount/my-account.php
+++ zenctuary-theme-starter/woocommerce/myaccount/my-account.php
@@ -7,11 +7,29 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$current_user = wp_get_current_user();
-$avatar_url   = zenctuary_get_account_avatar_url( get_current_user_id() );
+$current_user     = wp_get_current_user();
+$avatar_url       = zenctuary_get_account_avatar_url( get_current_user_id() );
+$current_endpoint = '';
+$account_url      = function_exists( 'wc_get_page_permalink' ) ? wc_get_page_permalink( 'myaccount' ) : home_url( '/my-account/' );
+
+if ( function_exists( 'WC' ) && WC()->query && method_exists( WC()->query, 'get_current_endpoint' ) ) {
+	$current_endpoint = (string) WC()->query->get_current_endpoint();
+}
+
+if ( '' === $current_endpoint ) {
+	foreach ( array_keys( wc_get_account_menu_items() ) as $endpoint ) {
+		if ( isset( $GLOBALS['wp']->query_vars[ $endpoint ] ) ) {
+			$current_endpoint = (string) $endpoint;
+			break;
+		}
+	}
+}
+
+$has_endpoint  = '' !== $current_endpoint && 'dashboard' !== $current_endpoint;
+$shell_classes = 'zen-account-shell' . ( $has_endpoint ? ' zen-account-shell--endpoint' : ' zen-account-shell--menu' );
 ?>
 
-<section class="zen-account-shell">
+<section class="<?php echo esc_attr( $shell_classes ); ?>">
 	<div class="zen-account-shell__inner">
 		<div class="zen-account-shell__panel">
 			<aside class="zen-account-shell__sidebar">
@@ -35,6 +53,10 @@ $avatar_url   = zenctuary_get_account_avatar_url( get_current_user_id() );
 			</aside>
 
 			<div class="zen-account-shell__content">
+				<a class="zen-account-shell__mobile-menu-link" href="<?php echo esc_url( $account_url ); ?>">
+					<span aria-hidden="true"><?php echo wp_kses_post( zenctuary_get_account_svg_icon( 'back' ) ); ?></span>
+					<?php esc_html_e( 'Back to account menu', 'zenctuary' ); ?>
+				</a>
 				<div class="zen-account-shell__content-scroll">
 					<?php do_action( 'woocommerce_account_content' ); ?>
 				</div>
